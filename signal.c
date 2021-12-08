ff9deaa0e630f7c1910602ef228c99040cbfa0d3
rafed01 <rafedriahi.rr@gmail.com>
2021-12-08 21:58:13 +0100

    signal

---
+++ signal.c
@@ -0,0 +1,15 @@
+#include"simple_shell.h"
+
+/**
+*sig - signal handler
+*@signal: input
+*/
+void sig(int signal)
+{
+	if (signal == SIGINT)
+	{
+		_putchar('\n');
+		_putchar('>');
+		_putchar(' ');
+	}
+}
